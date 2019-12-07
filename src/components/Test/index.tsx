c70ae4ff4b0f50c2320dacd54387090b9279a862
Cody Persinger <codypersinger@gmail.com>
2019-12-07 13:53:52 -0600

    WIP

--- src/components/Test/index.tsx
+++ src/components/Test/index.tsx
@@ -1,6 +1,6 @@
 import React, { useRef, useEffect } from 'react';
 import { ReactThreeFiber, useThree, useFrame, useRender, useUpdate, Canvas, extend } from 'react-three-fiber';
-import { PerspectiveCamera } from 'three';
+import { PerspectiveCamera, Geometry } from 'three';
 import { useCannon, PhysicsProvider } from 'Hooks';
 import { Vector2, Vector3 } from 'Types';
 import { CapsuleGeometry } from 'Primitives';
@@ -9,12 +9,13 @@ import * as CANNON from 'cannon';
 declare global {
 	namespace JSX {
 		interface IntrinsicElements {
-			capsuleGeometry: ReactThreeFiber.Object3DNode<CapsuleGeometry, typeof CapsuleGeometry>;
+			// capsuleGeometry: ReactThreeFiber.Object3DNode<CapsuleGeometry, typeof CapsuleGeometry>;
+			capsuleGeometry: any;
 		}
 	}
 }
 
-extend({ CapsuleGeometry })
+extend({ CapsuleGeometry });
 
 const Camera = (props: ReactThreeFiber.Object3DNode<PerspectiveCamera, typeof PerspectiveCamera>) => {
 	const cameraRef = useRef<PerspectiveCamera>({} as PerspectiveCamera);
@@ -35,10 +36,14 @@ const Camera = (props: ReactThreeFiber.Object3DNode<PerspectiveCamera, typeof Pe
 
 const Floor = ({
 	position,
-	size,
+	width,
+	height,
+	color,
 }: {
 	position: Vector3;
-	size: Vector2;
+	width: number;
+	height: number;
+	color: string;
 }) => {
 	const floorRef = useCannon({ mass: 0 }, body => {
 		body.addShape(new CANNON.Plane());
@@ -51,52 +56,75 @@ const Floor = ({
 		>
 			<planeBufferGeometry
 				attach="geometry"
-				args={size}
+				args={[ width, height ]}
 			/>
 			<meshPhongMaterial
 				attach="material"
-				color="#272727"
+				color={color}
 			/>
 		</mesh>
 	);
 };
 
+// Attempt to make capsule physics based on CANNON.Cylinder
+
 const Capsule = ({
-	position,
-	size,
+	position = [ 0, 0, 0 ],
+	radius = 1,
+	height = 2,
+	segments = 32,
 }: {
-	position: Vector3;
-	size: Vector3;
+	position?: Vector3;
+	radius?: number;
+	height?: number;
+	segments?: number;
 }) => {
-	// const capsuleRef = useCannon();
+	const capsuleRef = useCannon({ mass: 10 }, body => {
+		body.addShape(new CANNON.Cylinder(radius, radius, height, segments));
+		body.position.set(...position);
+	});
 
 	return (
-		<mesh>
-			<capsuleGeometry />
+		<mesh
+			castShadow
+			ref={capsuleRef}
+		>
+			<capsuleGeometry
+				attach="geometry"
+				args={[ radius, height, segments ]}
+			/>
+			<meshNormalMaterial
+				attach="material"
+				color="red"
+			/>
 		</mesh>
 	);
 };
 
-const Item = ({
-	position,
-	size,
+const Cube = ({
+	position = [ 0, 0, 0 ],
+	width = 1,
+	height = 1,
+	depth = 1,
 }: {
-	position: Vector3;
-	size: Vector3;
+	position?: Vector3;
+	width?: number;
+	height?: number;
+	depth?: number;
 }) => {
-	const itemRef = useCannon({ mass: 1000 }, body => {
-		body.addShape(new CANNON.Box(new CANNON.Vec3(...size.map(component => component * 0.5))));
+	const cubRef = useCannon({ mass: 10 }, body => {
+		body.addShape(new CANNON.Box(new CANNON.Vec3(width * 0.5, height * 0.5, depth * 0.5)));
 		body.position.set(...position);
 	});
 	return (
 		<mesh
 			castShadow
 			receiveShadow
-			ref={itemRef}
+			ref={cubRef}
 		>
 			<boxGeometry
 				attach="geometry"
-				args={size}
+				args={[ width, height, depth ]}
 			/>
 			<meshStandardMaterial attach="material" />
 		</mesh>
@@ -120,21 +148,47 @@ const Test = () => {
 					rotation={[ 1, 0, 0 ]}
 				/>
 				<PhysicsProvider>
-					<Item
+					<Cube
 						position={[ -0.5, -0.5, 1 ]}
-						size={[ 1, 2, 1 ]}
+						width={1}
+						height={2}
+						depth={1}
 					/>
-					<Item
+					<Cube
 						position={[ 0, 0, 5 ]}
-						size={[ 2, 1, 1 ]}
+						width={2}
+						height={1}
+						depth={1}
 					/>
-					<Item
+					<Cube
 						position={[ 0.5, 0.5, 7 ]}
-						size={[ 1, 1, 2 ]}
+						width={1}
+						height={1}
+						depth={2}
+					/>
+					<Capsule
+						position={[ 0, 0, 2 ]}
+						radius={0.5}
+						height={1}
+						segments={32}
+					/>
+					<Capsule
+						position={[ 0.5, 0.5, 6 ]}
+						radius={0.5}
+						height={1}
+						segments={32}
+					/>
+					<Capsule
+						position={[ 0, 1, 4 ]}
+						radius={0.5}
+						height={1}
+						segments={32}
 					/>
 					<Floor
 						position={[ 0, 0, 0 ]}
-						size={[ 10, 10 ]}
+						width={10}
+						height={10}
+						color="#272727"
 					/>
 				</PhysicsProvider>
 			</Canvas>
